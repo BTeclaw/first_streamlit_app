83472839bfe6e4d762a6fe9e7b6a046fa28853ee
@teclawbla <teclawbla@gmail.com>
2022-06-11 11:19:15 +0200

    next

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,4 +12,4 @@ streamlit.dataframe(fruits_to_show)
 
 
 fruityvice_response = rq.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
