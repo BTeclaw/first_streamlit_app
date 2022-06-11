9cd0d160d2cdcda530944d4eb3b04e0a73b1d6a5
@teclawbla <teclawbla@gmail.com>
2022-06-11 12:12:04 +0200

    next

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,16 @@
 import streamlit 
 import pandas as pd
 import requests as rq
-import snowflake.connector 
+import snowflake.connector
 
+
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
+my_data_row = my_cur.fetchall()
+streamlit.text("The fruit load list contains: ")
+streamlit.text(my_data_row)
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 my_fruit_list = my_fruit_list.set_index('Fruit')
